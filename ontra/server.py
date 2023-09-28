751a648aa137b2a8a49d011cc0781301bf00a825
Joshua Jesse Coronado <joshua@coronado.io>
2023-09-28 15:47:19 -0700

    initial commit

--- ontra/server.py
+++ ontra/server.py
@@ -6,11 +6,11 @@ app = FastAPI()
 
 
 @app.get("/")
-def read_root():
+def read_root() -> dict[str, int]:
     return {"The current epoch time": int(time.time())}
 
 
 # Perform a health check, and return a JSON response with the status
 @app.get("/health", status_code=200)
-def get_health():
+def get_health() -> dict[str, str]:
     return {"status": "ok"}
